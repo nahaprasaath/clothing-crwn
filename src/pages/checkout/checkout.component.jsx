b7b691ce58ad5bc71913f5fb36cc49ee6cc6332c
nahaprasaath <klu9914009075@gmail.com>
2021-05-23 17:31:30 +0530

    removing redux-logger from production build

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripe-button/stripe.button.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
-
+import {
+  selectCartItems,
+  selectCartTotal
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -34,11 +36,11 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
     <div className='total'>TOTAL: ${total}</div>
     <div className='test-warning'>
-      * Please use the following test credit card for Payment*
+      *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - EXP: 01/22 - CVV:123 
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
-    <StripeCheckoutButton price={total}/>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
